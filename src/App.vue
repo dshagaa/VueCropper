9709be55035053146cfd6d0d9379089641bcc09f
dshagaa <ing.dx16@gmail.com>
2018-03-10 12:38:34 -0600

    Init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!--img src="./assets/logo.png"-->
+    <!--HelloWorld/-->
+    <vue-cropper :auto-crop='true'></vue-cropper>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import VueCroper from './components/VueCropper'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    'vue-cropper':VueCroper
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VueCropper.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<img :src="src" alt="" ref="cropper" width="15%">
+	</div>
+</template>
+ 
+<script>
+	import Cropper from 'cropperjs';
+	export default {
+		name:'vue-cropper',
+		props: {
+			autoCrop: {
+				type: Boolean,
+				default: false
+			},
+			src: {
+				type: String,
+				default: require('../assets/logo.png')
+			}
+		},
+		created() {},
+		beforeMount() {},
+		mounted() {},
+		methods: {
+			init() {
+				new Cropper(this.$refs.cropper,this.options)
+			},
+			crop() {
+				this.$refs.cropper.crop();
+			}
+		},
+		data() {
+			return {
+				options:{
+					autoCrop: this.autoCrop
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+</style>
